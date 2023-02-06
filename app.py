7ee9ca433809ebcdc41d6ae39d2eaa47918850eb
ageVega <agevega@hotmail.com>
2023-02-06 23:21:09 +0100

    Added dev code

--- app.py
+++ app.py
@@ -123,7 +123,12 @@ def get_user(id):
 def home():
     return send_file('static/index.html')
 
-
+"""
 if __name__ == "__main__":
     from waitress import serve
     serve(app, host="0.0.0.0", port=8080)
+
+"""
+
+if __name__ == '__main__':
+    app.run(debug=True)
