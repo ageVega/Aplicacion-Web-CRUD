b948f9530378cb08d25c6183f22f4f4370a2d282
ageVega <agevega@hotmail.com>
2023-03-20 05:23:43 +0100

    Divided into api.py y app.py

---
+++ api.py
@@ -0,0 +1,107 @@
+# api.py
+import os
+from psycopg2 import connect, extras
+from flask import Blueprint, jsonify, request
+
+api = Blueprint('api', __name__)
+
+# Accede a las variables de entorno
+host = os.environ.get('DB_HOST')
+port = os.environ.get('DB_PORT')
+dbname = os.environ.get('DB_DATABASE')
+username = os.environ.get('DB_USER')
+password = os.environ.get('DB_PASSWORD')
+
+def get_connection():
+    conn = connect(host=host, port=port, dbname=dbname, user=username, password=password)
+    return conn
+
+@api.route('/api/tasks', methods=['GET'])
+def get_tasks():
+    conn = get_connection()
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+
+    cur.execute('SELECT * FROM tasks')
+    tasks = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return jsonify(tasks)
+
+@api.route('/api/tasks', methods=['POST'])
+def create_task():
+    new_task = request.get_json()
+    task = new_task['task']
+    priority = new_task['priority']
+
+    conn = get_connection()
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+
+    cur.execute('INSERT INTO tasks (task, priority) VALUES (%s, %s) RETURNING *',
+                (task, priority))
+
+    new_created_task = cur.fetchone()
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    return jsonify(new_created_task)
+
+@api.route('/api/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    conn = get_connection()
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+
+    cur.execute('DELETE FROM tasks WHERE id = %s RETURNING *', (id,))
+    task = cur.fetchone()
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    if task is None:
+        return jsonify({'message': 'Task not found'}), 404
+
+    return jsonify(task)
+
+@api.route('/api/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    new_task = request.get_json()
+    task = new_task['task']
+    priority = new_task['priority']
+
+    conn = get_connection()
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+
+    cur.execute('UPDATE tasks SET task = %s, priority = %s WHERE id = %s RETURNING *',
+                (task, priority, id))
+    updated_task = cur.fetchone()
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    if updated_task is None:
+        return jsonify({'message': 'Task not found'}), 404
+
+    return jsonify(updated_task)
+
+@api.route('/api/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    conn = get_connection()
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+
+    cur.execute('SELECT * FROM tasks WHERE id = %s', (id,))
+    task = cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+    if task is None:
+        return jsonify({'message': 'Task not found'}), 404
+
+    return jsonify(task)
--- app.py
+++ app.py
@@ -1,130 +1,17 @@
-from flask import Flask, request, jsonify, send_file
-from psycopg2 import connect, extras
+# app.py
+import os
 from dotenv import load_dotenv
-from os import environ
+from flask import Flask, send_file
+from api import api
 
-load_dotenv()
+load_dotenv()  # Carga las variables de entorno desde .env
 
 app = Flask(__name__)
+app.register_blueprint(api)
 
-host = environ.get('DB_HOST')
-port = environ.get('DB_PORT')
-dbname = environ.get('DB_DATABASE')
-username = environ.get('DB_USER')
-password = environ.get('DB_PASSWORD')
-
-def get_connection():
-    conn = connect(host=host, port=port, dbname=dbname, user=username, password=password)
-    return conn
-
-
-# Get tareas
-@app.get('/api/tasks')
-def get_tasks():
-    conn = get_connection()
-    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
-
-    cur.execute('SELECT * FROM tasks')
-    tasks = cur.fetchall()
-
-    cur.close()
-    conn.close()
-
-    return jsonify(tasks)
-
-# Create tarea
-@app.post('/api/tasks')
-def create_task():
-    new_task = request.get_json()
-    task = new_task['task']
-    priority = new_task['priority']
-
-    conn = get_connection()
-    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
-
-    cur.execute('INSERT INTO tasks (task, priority) VALUES (%s, %s) RETURNING *',
-                (task, priority))
-
-    new_created_task = cur.fetchone()
-    conn.commit()
-
-    cur.close()
-    conn.close()
-
-    return jsonify(new_created_task)
-
-# Delete tarea
-@app.delete('/api/tasks/<id>')
-def delete_task(id):
-    conn = get_connection()
-    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
-
-    cur.execute('DELETE FROM tasks WHERE id = %s RETURNING *', (id,))
-    task = cur.fetchone()
-
-    conn.commit()
-
-    cur.close()
-    conn.close()
-
-    if task is None:
-        return jsonify({'message': 'Task not found'}), 404
-
-    return jsonify(task)
-
-# Edit tarea
-@app.put('/api/tasks/<id>')
-def update_task(id):
-    new_task = request.get_json()
-    task = new_task['task']
-    priority = new_task['priority']
-
-    conn = get_connection()
-    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
-
-    cur.execute('UPDATE tasks SET task = %s, priority = %s WHERE id = %s RETURNING *',
-                (task, priority, id))
-    updated_task = cur.fetchone()
-
-    conn.commit()
-
-    cur.close()
-    conn.close()
-
-    if updated_task is None:
-        return jsonify({'message': 'Task not found'}), 404
-
-    return jsonify(updated_task)
-
-# Get tarea
-@app.get('/api/tasks/<id>')
-def get_task(id):
-    conn = get_connection()
-    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
-
-    cur.execute('SELECT * FROM tasks WHERE id = %s', (id,))
-    task = cur.fetchone()
-
-    cur.close()
-    conn.close()
-
-    if task is None:
-        return jsonify({'message': 'Task not found'}), 404
-
-    return jsonify(task)
-
-# Pagina principal
-@app.get('/')
+@app.route('/')
 def home():
     return send_file('static/index.html')
 
-
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
-
-"""
-
 if __name__ == '__main__':
     app.run(debug=True)
-"""
