f69c12b8da90160ff9ec11b2676a05dac189dce4
ageVega <agevega@hotmail.com>
2023-03-20 06:24:38 +0100

    Inicio de sesion

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 from flask import Flask, send_file
 from flask_login import LoginManager, current_user
 from api import api
-from login import login_blueprint, get_user
+from login import login_blueprint
+from login import login_manager  # Importa la instancia de LoginManager desde login.py
 
 load_dotenv()  # Carga las variables de entorno desde .env
 
@@ -13,9 +14,9 @@ app.secret_key = os.environ.get('SECRET_KEY')  # Necesario para flask-login
 app.register_blueprint(api)
 app.register_blueprint(login_blueprint, url_prefix='/auth')
 
-login_manager = LoginManager()
+# login_manager = LoginManager()  # Elimina la creación de una segunda instancia
 login_manager.init_app(app)
-login_manager.login_view
+login_manager.login_view = "auth.login"  # Establece la vista de inicio de sesión
 
 @app.route('/')
 def home():
--- login.py
+++ login.py
@@ -1,18 +1,19 @@
 # login.py
-import os
+from flask import Blueprint, request, redirect, url_for, jsonify, render_template
 from psycopg2 import connect, extras
-from flask import Blueprint, request, redirect, url_for, jsonify
+from os import environ
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, logout_user, login_required, UserMixin
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required
 
-login_blueprint = Blueprint('login', __name__)
+login_blueprint = Blueprint('auth', __name__)
 
-# Accede a las variables de entorno
-host = os.environ.get('DB_HOST')
-port = os.environ.get('DB_PORT')
-dbname = os.environ.get('DB_DATABASE')
-username = os.environ.get('DB_USER')
-password = os.environ.get('DB_PASSWORD')
+host = environ.get('DB_HOST')
+port = environ.get('DB_PORT')
+dbname = environ.get('DB_DATABASE')
+username = environ.get('DB_USER')
+password = environ.get('DB_PASSWORD')
+
+login_manager = LoginManager()
 
 def get_connection():
     conn = connect(host=host, port=port, dbname=dbname, user=username, password=password)
@@ -24,56 +25,81 @@ class User(UserMixin):
         self.username = username
         self.password = password
 
-def get_user(user_id):
+def get_user_by_id(user_id):
     conn = get_connection()
     cur = conn.cursor(cursor_factory=extras.RealDictCursor)
-    cur.execute('SELECT * FROM users WHERE id = %s', (user_id,))
+
+    cur.execute("SELECT * FROM users WHERE id = %s", (user_id,))
     user_data = cur.fetchone()
+
     cur.close()
     conn.close()
 
     if user_data:
         return User(user_data['id'], user_data['username'], user_data['password'])
-    else:
-        return None
+
+    return None
+
+@login_manager.user_loader
+def load_user(user_id):
+    return get_user_by_id(user_id)
 
 @login_blueprint.route('/register', methods=['POST'])
 def register():
-    username = request.form['username']
-    password = request.form['password']
+    username = request.form.get('username')
+    password = request.form.get('password')
+
     hashed_password = generate_password_hash(password)
 
     conn = get_connection()
     cur = conn.cursor(cursor_factory=extras.RealDictCursor)
-    cur.execute('INSERT INTO users (username, password) VALUES (%s, %s) RETURNING id', (username, hashed_password))
-    user_id = cur.fetchone()['id']
-    conn.commit()
+
+    try:
+        cur.execute('INSERT INTO users (username, password) VALUES (%s, %s) RETURNING id, username', (username, hashed_password))
+        user_data = cur.fetchone()
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        return jsonify({'message': str(e)}), 400
+
     cur.close()
     conn.close()
 
-    return jsonify({"id": user_id, "username": username})
+    user = User(user_data['id'], user_data['username'], hashed_password)
+    login_user(user)
+    return redirect(url_for('api.get_tasks'))
 
 @login_blueprint.route('/login', methods=['POST'])
 def login():
-    username = request.form['username']
-    password = request.form['password']
+    username = request.form.get('username')
+    password = request.form.get('password')
 
     conn = get_connection()
     cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+
     cur.execute('SELECT * FROM users WHERE username = %s', (username,))
     user_data = cur.fetchone()
+
     cur.close()
     conn.close()
 
-    if user_data and check_password_hash(user_data['password'], password):
-        user = User(user_data['id'], user_data['username'], user_data['password'])
-        login_user(user)
-        return redirect(url_for('api.get_tasks'))
-    else:
-        return jsonify({"error": "Invalid username or password"}), 401
+    if not user_data or not check_password_hash(user_data['password'], password):
+        return jsonify({'message': 'Invalid username or password'}), 401
 
-@login_blueprint.route('/logout', methods=['GET'])
+    user = User(user_data['id'], user_data['username'], user_data['password'])
+    login_user(user)
+    return redirect(url_for('api.get_tasks'))
+
+@login_blueprint.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('api.home'))
+    return redirect(url_for('api.get_tasks'))
+
+@login_blueprint.route('/register', methods=['GET'])
+def register_form():
+    return render_template('register.html')
+
+@login_blueprint.route('/login', methods=['GET'])
+def login_form():
+    return render_template('login.html')
