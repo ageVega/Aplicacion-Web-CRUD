39fef845a4ec12ba722c763780447453ab9776c0
ageVega <agevega@hotmail.com>
2023-03-23 17:08:46 +0100

    Listo para prod

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def home():
     return render_template('home.html')
 
 
-"""
+
 if __name__ == "__main__":
     from waitress import serve
     serve(app, host="0.0.0.0", port=8080)
@@ -46,3 +46,4 @@ if __name__ == "__main__":
 """
 if __name__ == '__main__':
     app.run(debug=True)
+"""
