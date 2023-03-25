6846fb7ba31888326ba83e1a338fa0b92b2aef6a
ageVega <agevega@hotmail.com>
2023-03-25 18:30:50 +0100

    Servido desde waitress

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ login_manager.login_view = "auth.login"  # Establece la vista de inicio de sesi√
 @app.route('/dashboard')
 @login_required
 def dashboard():
-    username = session.get('username', 'Invitado')
-    return render_template('dashboard.html', username=username)
+    house_name = session.get('house_name', 'Invitado')
+    return render_template('dashboard.html', house_name=house_name)
 
 
 
@@ -38,7 +38,7 @@ def home():
     return render_template('home.html')
 
 
-
+"""
 if __name__ == "__main__":
     from waitress import serve
     serve(app, host="0.0.0.0", port=8080)
@@ -46,4 +46,3 @@ if __name__ == "__main__":
 """
 if __name__ == '__main__':
     app.run(debug=True)
-"""
