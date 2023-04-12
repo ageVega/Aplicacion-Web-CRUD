73d2368b9a44260d50106dae00b1f01eadbdc2f5
Alejandro Vega Vaquero <agevega@hotmail.com>
2023-04-12 12:49:28 +0200

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,18 +1,18 @@
 # Configura el proveedor AWS
 provider "aws" {
-  region     = "eu-west-1"
+  region = "eu-west-1"
 }
 
 # Crea un VPC
-resource "aws_vpc" "mi_vpc_test" {
+resource "aws_vpc" "terraform_test" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "mi-vpc"
+    Name = "terraform_test"
   }
 }
 
 # Muestra la ID del VPC como salida
 output "vpc_id" {
-  value = aws_vpc.mi_vpc_test.id
+  value = aws_vpc.terraform_test.id
 }
