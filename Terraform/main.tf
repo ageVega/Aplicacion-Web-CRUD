01487b7d6f2433e05f1df8eaa4f0a81aa791af72
Alejandro Vega Vaquero <agevega@hotmail.com>
2023-04-12 12:24:03 +0200

    Credentials en .aws/

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,12 +1,10 @@
 # Configura el proveedor AWS
 provider "aws" {
-  region     = "us-west-2"
-  access_key = "<tu_access_key>"
-  secret_key = "<tu_secret_key>"
+  region     = "eu-west-1"
 }
 
 # Crea un VPC
-resource "aws_vpc" "mi_vpc" {
+resource "aws_vpc" "mi_vpc_test" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
@@ -16,5 +14,5 @@ resource "aws_vpc" "mi_vpc" {
 
 # Muestra la ID del VPC como salida
 output "vpc_id" {
-  value = aws_vpc.mi_vpc.id
+  value = aws_vpc.mi_vpc_test.id
 }
