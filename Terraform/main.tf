1f6792f87c259641c2fa6852d60efe6b120fa507
Alejandro Vega Vaquero <agevega@hotmail.com>
2023-04-13 15:21:34 +0200

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -101,12 +101,22 @@ resource "aws_security_group" "matrix_sg" {
   vpc_id      = module.vpc.vpc_id
 }
 
-resource "aws_security_group_rule" "matrix_sg_ingress" {
+resource "aws_security_group_rule" "matrix_sg_ingress_http" {
   security_group_id = aws_security_group.matrix_sg.id
 
   type        = "ingress"
-  from_port   = 0
-  to_port     = 0
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "matrix_sg_ingress_https" {
+  security_group_id = aws_security_group.matrix_sg.id
+
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 }
@@ -117,7 +127,7 @@ resource "aws_security_group_rule" "matrix_sg_egress" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
-  protocol    = "tcp"
+  protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 }
 
