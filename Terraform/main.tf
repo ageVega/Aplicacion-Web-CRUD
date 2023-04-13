4a8b8a9a33ab9afc21a352687d146f28f017d2e6
Alejandro Vega Vaquero <agevega@hotmail.com>
2023-04-13 16:43:39 +0200

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -102,12 +102,42 @@ resource "aws_security_group" "matrix_sg" {
   vpc_id      = module.vpc.vpc_id
 }
 
-resource "aws_security_group_rule" "matrix_sg_ingress_all" {
+resource "aws_security_group_rule" "matrix_sg_ingress_ssh" {
   security_group_id = aws_security_group.matrix_sg.id
 
   type        = "ingress"
-  from_port   = 0
-  to_port     = 65535
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "matrix_sg_ingress_http" {
+  security_group_id = aws_security_group.matrix_sg.id
+
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "matrix_sg_ingress_https" {
+  security_group_id = aws_security_group.matrix_sg.id
+
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "matrix_sg_ingress_app" {
+  security_group_id = aws_security_group.matrix_sg.id
+
+  type        = "ingress"
+  from_port   = 8080
+  to_port     = 8080
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 }
