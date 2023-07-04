9492aee74430faadb5d0757b43acf9e808742b6d
Alejandro Vega Vaquero <agevega@hotmail.com>
2023-07-04 17:25:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,6 @@ def dashboard():
     return render_template('dashboard.html', house_name=house_name)
 
 
-"""
 if __name__ == "__main__":
     from waitress import serve
     serve(app, host="0.0.0.0", port=8080)
@@ -42,3 +41,5 @@ if __name__ == "__main__":
 """
 if __name__ == '__main__':
     app.run(debug=True)
+
+"""
