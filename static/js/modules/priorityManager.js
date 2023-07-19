ad3f23e9973a3e0ff620aaa56424c1fb6197de1c
Alejandro Vega Vaquero <agevega@hotmail.com>
2023-07-19 02:28:21 +0200

    Esqueleto modulos js

---
+++ static/js/modules/priorityManager.js
@@ -0,0 +1,19 @@
+export function priorityFormEvent() {
+    // Código para manejar el formulario de prioridades
+}
+
+export function resetPriorityEvent() {
+    // Código para manejar el reseteo de prioridades
+}
+
+export function weekdayPriorityEvent() {
+    // Código para manejar el establecimiento de prioridades de días de semana
+}
+
+export function clearPriorityNameForm() {
+    // Código para limpiar el formulario de prioridad
+}
+
+export function reloadPage() {
+    // Código para recargar la página
+}
---
+++ static/js/modules/taskManager.js
@@ -0,0 +1,23 @@
+export function taskFormEvent() {
+    // Código para manejar el formulario de tareas
+}
+
+export function deleteTaskEvent() {
+    // Código para manejar la eliminación de tareas
+}
+
+export function editTaskEvent() {
+    // Código para manejar la edición de tareas
+}
+
+export function renderTask() {
+    // Código para renderizar tareas
+}
+
+export function sortTasks() {
+    // Código para ordenar tareas
+}
+
+export function updatePrioritySelect() {
+    // Código para actualizar select de prioridad
+}
