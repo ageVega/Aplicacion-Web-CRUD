a6b6b453ab11a2be13c2608926389b7657f6e9af
Alejandro Vega Vaquero <agevega@hotmail.com>
2023-07-23 12:05:43 +0200

    Checkpoint js

--- static/js/main.js
+++ static/js/main.js
@@ -1,316 +1,44 @@
 // main.js
-const houseId = '{{current_user.id}}'; 
-const deleteHouseButton = document.getElementById('delete-house-btn');
+import * as TaskManager     from './modules/taskManager.js';
+import * as PriorityManager from './modules/priorityManager.js';
+import * as UserManager     from './modules/userManager.js';
 
-const taskForm               = document.querySelector('#taskForm');
-const taskList               = document.querySelector('#taskList') ? document.querySelector('#taskList') : null;
-const priorityNameForm       = document.querySelector('#priorityNameForm');
-const resetPriorityNamesForm = document.querySelector('#resetPriorityNamesForm');
-const setWeekdayNamesForm    = document.querySelector('#setWeekdayNamesForm');
+import { editing, tareaId } from './modules/taskManager.js'; 
 
-let prioritySelect = document.querySelector('select[name="prioridad"]');
+export const houseId = sessionStorage.getItem('house_id');
 
-let tareas = [];
-let priorityNames = [];
+export let tareas = [];
+export let priorityNames = [];
 
-let editing = false;
-let tareaId = null;
+async function initializeApp() {
+    UserManager.deleteHouseButtonEvent();
 
+    TaskManager.taskFormEvent(tareas, priorityNames, editing, tareaId, houseId);
+    TaskManager.sortTasks(tareas);
+
+    PriorityManager.priorityFormEvent(priorityNames, houseId);
+    PriorityManager.resetPriorityEvent();
+    PriorityManager.setWeekdayNamesEvent();
+    PriorityManager.clearPriorityNameForm();
+}
 
 window.addEventListener('DOMContentLoaded', async () => {
-    clearHouseIdOnLogout();
+    await initializeApp();
+
+    UserManager.clearHouseIdOnLogout();
+
+    const taskList = document.querySelector('#taskList') ? document.querySelector('#taskList') : null;
     if (taskList) {  
         const responseTasks = await fetch(`/api/tasks?house_id=${houseId}`);
         const dataTasks = await responseTasks.json();
         tareas = dataTasks;
-    }
-
-    const responsePriorities = await fetch(`/api/priority_levels?house_id=${houseId}`);
-    const dataPriorities = await responsePriorities.json();
-    priorityNames = dataPriorities;
     
-    updatePrioritySelect(priorityNames);  
-
-    if (taskList) {  
-        renderTask(tareas);
+        const responsePriorities = await fetch(`/api/priority_levels?house_id=${houseId}`);
+        responsePriorities.json().then(dataPriorities => {
+            priorityNames = dataPriorities;    
+            PriorityManager.updatePrioritySelect(priorityNames);
+            TaskManager.renderTask(tareas, priorityNames);
+        });
     }
+    
 });
-
-if (taskForm) {
-    taskForm.addEventListener('submit', async (e) => {
-        e.preventDefault();
-
-        const tarea = taskForm['tarea'].value;
-        const prioridad = taskForm['prioridad'].value;
-        const house_id = houseId;
-
-        if (!editing) {
-            const response = await fetch('/api/tasks', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    task: tarea,
-                    priority: prioridad,
-                    house_id
-                })
-            });
-
-            const newTask = await response.json();
-            tareas.push(newTask);
-        } else {
-            const response = await fetch(`/api/tasks/${tareaId}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    task: tarea,
-                    priority: prioridad,
-                })
-            });
-
-            const updatedTask = await response.json();
-            tareas = tareas.map(tarea => tarea.id === updatedTask.id ? updatedTask : tarea);
-            editing = false;
-        }
-
-        taskForm.reset();
-        renderTask(tareas);
-    });
-}
-
-if (deleteHouseButton) {
-    deleteHouseButton.addEventListener('click', async function() {
-        const houseId = this.getAttribute('data-house-id');
-        if (confirm('¿Estás seguro de que quieres eliminar esta casa? Esta acción no se puede deshacer.')) {
-            const response = await fetch(`/auth/cancel/${houseId}`, {
-                method: 'DELETE'
-            });
-            const data = await response.json();
-            if (response.ok) {
-                alert(data.message);
-                window.location.href = '/home';
-            } else {
-                alert('Error: ' + data.message);
-            }
-        }
-    });
-}
-
-if (priorityNameForm) { 
-    priorityNameForm.addEventListener('submit', async (e) => {
-        e.preventDefault();
-
-        const level = priorityNameForm['priorityLevel'].value;
-        const name = priorityNameForm['priorityName'].value;
-        const house_id = houseId;
-
-        const response = await fetch(`/api/priority_names/${level}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                name,
-                house_id
-            })
-        });
-
-        const updatedPriorityName = await response.json();
-
-        priorityNames = priorityNames.map(p => p.level === updatedPriorityName.level ? updatedPriorityName : p);
-
-        // Reasigna prioritySelect antes de llamar a updatePrioritySelect
-        prioritySelect = document.querySelector('select[name="prioridad"]');
-        updatePrioritySelect(priorityNames);
-        reloadPage();
-    
-        // Limpia el formulario
-        clearPriorityNameForm();
-    });
-}
-
-if (resetPriorityNamesForm) {
-    resetPriorityNamesForm.addEventListener('submit', async (event) => {
-        // Previene el comportamiento por defecto del formulario (recarga de la página)
-        event.preventDefault();
-
-        // Lógica para resetear los nombres de prioridad
-        const response = await fetch('/api/reset_priority_names', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-
-        if (response.ok) {
-            const updatedPriorityNames = await response.json();
-
-            // Actualiza las prioridades en el cliente
-            priorityNames = updatedPriorityNames;
-
-            // Reasigna prioritySelect antes de llamar a updatePrioritySelect
-            prioritySelect = document.querySelector('select[name="prioridad"]');
-            updatePrioritySelect(priorityNames);
-
-            // Redirecciona al dashboard
-            window.location.href = '/dashboard';
-        }
-    });
-}
-
-if (setWeekdayNamesForm) {
-    setWeekdayNamesForm.addEventListener('submit', async (event) => {
-        // Previene el comportamiento por defecto del formulario (recarga de la página)
-        event.preventDefault();
-
-        // Lógica para establecer los nombres de prioridad a los días de la semana
-        const response = await fetch('/api/set_weekday_priority_names', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-
-        if (response.ok) {
-            const updatedPriorityNames = await response.json();
-
-            // Actualiza las prioridades en el cliente
-            priorityNames = updatedPriorityNames;
-
-            // Reasigna prioritySelect antes de llamar a updatePrioritySelect
-            prioritySelect = document.querySelector('select[name="prioridad"]');
-            updatePrioritySelect(priorityNames);
-
-            // Redirecciona al dashboard
-            window.location.href = '/dashboard';
-        }
-    });
-}
-
-function clearPriorityNameForm() {
-    // Establece el valor de select y input a vacío
-    priorityNameForm['priorityLevel'].value = '';
-    priorityNameForm['priorityName'].value = '';
-    console.log('Formulario limpiado'); // Añade esta línea
-}
-
-async function reloadPage() {
-    const response = await fetch('/api/tasks');
-    const data = await response.json();
-    tareas = data;
-    
-    if (taskList) {
-        renderTask(tareas);
-    }
-}
-
-function renderTask(tareas) {
-    if (!taskList) {
-        return;
-    }
-
-    taskList.innerHTML = '';
-    tareas = sortTasks(tareas, true);
-
-    tareas.forEach(tarea => {
-        const taskItem = document.createElement('li');
-        taskItem.classList = 'list-group-item list-group-item-dark my-2';
-        const priorityName = priorityNames.find(p => p.level === tarea.priority).name;
-        taskItem.innerHTML = `
-            <header class="d-flex justify-content-between align-items-center">
-                <h3>${tarea.priority}. ${priorityName}</h3>
-                <div>
-                    <button class="btn-edit btn btn-secondary btn-sm">edit</button>
-                    <button class="btn-delete btn btn-danger btn-sm">delete</button>
-                </div>
-            </header>
-            <p>${tarea.task}</p>
-        `;
-
-        const btnDelete = taskItem.querySelector('.btn-delete');
-
-        btnDelete.addEventListener("click", async () => {
-            const response = await fetch(`/api/tasks/${tarea.id}`, {
-                method: 'DELETE'
-            });
-            const data = await response.json();
-
-            tareas = tareas.filter(tarea => tarea.id !== data.id);
-
-            renderTask(tareas);
-        });
-
-        const btnEdit = taskItem.querySelector('.btn-edit');
-
-        btnEdit.addEventListener("click", async () => {
-            const response = await fetch(`/api/tasks/${tarea.id}`);
-            const data = await response.json();
-
-            taskForm["tarea"].value = data.task;
-            taskForm["prioridad"].value = data.priority;
-
-            editing = true;
-            tareaId = data.id;
-        });
-
-        taskList.append(taskItem);
-    });
-}
-
-function sortTasks(tareas, sortByPriority = true) {
-    return tareas.sort((a, b) => {
-        if (sortByPriority) {
-            if (a.priority < b.priority) return -1;
-            if (a.priority > b.priority) return 1;
-        }
-        if (a.task < b.task) return -1;
-        if (a.task > b.task) return 1;
-        return 0;
-    });
-}
-
-function clearHouseIdOnLogout() {
-    const urlPath = window.location.pathname;
-    if (urlPath === '/' || urlPath === '/home') {
-        sessionStorage.removeItem('user_id');
-    }
-}
-
-function priorityText(priority) {
-    const priorityName = priorityNames.find(p => p.level === parseInt(priority)).name;
-    return priorityName || '';
-}
-
-function updatePriorityNames(priorityNames) {
-    const selectPriority = taskForm['prioridad'];
-    selectPriority.innerHTML = '';
-    priorityNames.forEach(priority => {
-        const option = document.createElement('option');
-        option.value = priority.level;
-        option.textContent = priority.name;
-        selectPriority.appendChild(option);
-    });
-}
-
-function updatePrioritySelect(priorityNames) {
-    // Mover la asignación de prioritySelect aquí
-    let prioritySelect = document.querySelector('select[name="prioridad"]');
-
-    if (!prioritySelect) {
-        // No intentar actualizar el select si no existe.
-        return;
-    }
-    
-    priorityNames.sort((a, b) => a.level - b.level);  // Añadir esta línea para ordenar las prioridades por su nivel
-    
-    prioritySelect.innerHTML = '';
-    priorityNames.forEach(priority => {
-        const option = document.createElement('option');
-        option.value = priority.level;
-        option.textContent = `${priority.level}. ${priority.name}`;
-        prioritySelect.appendChild(option);
-    });
-}
--- static/js/modules/userManager.js
+++ static/js/modules/userManager.js
@@ -1,3 +1,28 @@
+// userManager.js
 export function clearHouseIdOnLogout() {
-    // Código para manejar el logout de usuario
+    const urlPath = window.location.pathname;
+    if (urlPath === '/' || urlPath === '/home') {
+        sessionStorage.removeItem('user_id');
+    }
+}
+
+export function deleteHouseButtonEvent() {
+    const deleteHouseButton = document.getElementById('delete-house-btn');
+    if (!deleteHouseButton) return;
+
+    deleteHouseButton.addEventListener('click', async function() {
+        const houseId = this.getAttribute('data-house-id');
+        if (confirm('¿Estás seguro de que quieres eliminar esta casa? Esta acción no se puede deshacer.')) {
+            const response = await fetch(`/auth/cancel/${houseId}`, {
+                method: 'DELETE'
+            });
+            const data = await response.json();
+            if (response.ok) {
+                alert(data.message);
+                window.location.href = '/home';
+            } else {
+                alert('Error: ' + data.message);
+            }
+        }
+    });
 }
