33478868c2946f7c7a7f3358224e65b3d5aca4dc
Alejandro Vega Vaquero <agevega@hotmail.com>
2023-07-30 19:44:52 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -47,6 +47,7 @@ def config():
     return render_template('config.html', house_name=house_name)
 
 
+"""
 if __name__ == "__main__":
     from waitress import serve
     serve(app, host="0.0.0.0", port=8080)
@@ -55,4 +56,3 @@ if __name__ == "__main__":
 if __name__ == '__main__':
     app.run(debug=True)
 
-"""
