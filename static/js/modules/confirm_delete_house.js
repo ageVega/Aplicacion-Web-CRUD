a89b325ba30b3bcef01cc34fa6849f6377a3f041
Alejandro Vega Vaquero <agevega@hotmail.com>
2023-08-04 17:52:32 +0200

    Update confirm_delete_house.js

--- static/js/modules/confirm_delete_house.js
+++ static/js/modules/confirm_delete_house.js
@@ -1,61 +1,50 @@
 // confirm_delete_house.js
 
-// Formularios en confirm_delete_house.html
 export function deleteHouseButtonEvent() {
-    const deleteHouseButton = document.getElementById('delete-house-btn');
-    if (!deleteHouseButton) return;
+    const deleteForm = document.getElementById('delete-form');
+    if (!deleteForm) return;
 
-    deleteHouseButton.addEventListener('click', async function() {
-        const houseId = this.getAttribute('data-house-id');
-        if (confirm('¿Estás seguro de que quieres eliminar esta casa? Esta acción no se puede deshacer.')) {
-            const response = await fetch(`/auth/cancel/${houseId}`, {
-                method: 'DELETE'
-            });
-            const data = await response.json();
+    deleteForm.addEventListener('submit', async function(event) {
+        event.preventDefault();
 
-            if (response.ok) {
-                alert(data.message);
-                window.location.href = '/home';
-            } else {
-                alert('Error: ' + data.message);
-            }
+        const password = document.getElementById('password').value;
+        const houseId = sessionStorage.getItem('house_id');
+
+        // Primero, confirma la contraseña
+        const confirmResponse = await fetch(`/auth/confirm_password/${houseId}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ password: password }),
+        });
+
+        const confirmData = await confirmResponse.json();
+
+        if (!confirmResponse.ok) {
+            alert('Error: ' + confirmData.message);
+            return;
         }
+
+        // Si la contraseña es correcta, pregúntale al usuario si realmente quiere eliminar la casa
+        const deleteConfirmed = confirm('¿Estás seguro de que quieres eliminar esta casa? Esta acción no se puede deshacer.');
+        
+        if (!deleteConfirmed) {
+            return;
+        }
+
+        // Finalmente, realiza la petición para eliminar la casa
+        const deleteResponse = await fetch(`/auth/cancel/${houseId}`, {
+            method: 'DELETE'
+        });
+        const deleteData = await deleteResponse.json();
+
+        if (!deleteResponse.ok) {
+            alert('Error: ' + deleteData.message);
+            return;
+        }
+
+        alert(deleteData.message);
+        window.location.href = '/home';
     });
 }
-
-document.getElementById('delete-form').addEventListener('submit', async function(event) {
-    event.preventDefault();
-
-    const password = document.getElementById('password').value;
-    const houseId = sessionStorage.getItem('house_id');
-
-    const response = await fetch(`/auth/confirm_password/${houseId}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ password: password }),
-    });
-
-    const data = await response.json();
-
-    if (response.ok) {
-        const deletionConfirmed = confirm('¿Estás seguro de que quieres eliminar esta casa? Esta acción no se puede deshacer.');
-        
-        if (deletionConfirmed) {
-            const deletionResponse = await fetch(`/auth/cancel/${houseId}`, {
-                method: 'DELETE'
-            });
-            const deletionData = await deletionResponse.json();
-
-            if (deletionResponse.ok) {
-                alert(deletionData.message);
-                window.location.href = '/home';
-            } else {
-                alert('Error: ' + deletionData.message);
-            }
-        }
-    } else {
-        alert('Error: ' + data.message);
-    }
-});
